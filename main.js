71b018662008605891bffe5a69f3bf6483c08e62
Jen Nguyen <jenniferannnguyen@live.com>
2023-06-08 15:07:36 -0600

    Feat: add updatePlayerTurnText function

--- main.js
+++ main.js
@@ -1,23 +1,41 @@
 var boxes = document.querySelectorAll('.boxes');
-var hibiscusEmoji = document.querySelectorAll('.hibiscus-1', '.hibiscus-2')
-var gameBoard = document.getElementById
+console.log('boxes:', boxes)
+var message = document.getElementById('whose-turn');
+console.log(message)
 
 
-boxes.addEventListener('click', function(){});
-window.addEventListener('load', function() {
-  player 1 =createPlayer(player1, '🌺', 0);
-  player 2 = createPlayer(player2, '🍄', 0);
-  startGame()
-})
+//on click check player turn - always player1 first
+//render innterhtml with the correct players token
+//call check wins function 
+//logic checking win combo's 
+//if it matches one of the array 
+// innertext who won
+//update win on data model/show on dom 
 
-var player1Symbol = '🌺';
-var player2Symbol = '🍄';
+var player1;
+var player2;
+var playersTurn = player1
 var gameOver = false; 
-var currentPlayer = createPlayer(player, token, wins {
-  player: player,
-  token: token,
-  wins: wins 
+
+// boxes.addEventListener('click', function(){});
+window.addEventListener('load', function() {
+  player1 = createPlayer('player1', '🌺', 0);
+  player2 = createPlayer('player2', '🍄', 0);
+  console.log('player1:', player1)
+  console.log('player2', player2)
+  updatePlayerTurnText()
 })
+
+for (var i = 0; i < boxes.length; i++) {
+  boxes[i].addEventListener('click', function(event) {
+    checkPlayerTurn()
+  })
+}
+// var currentPlayer = createPlayer(player, token, wins {
+//   player: player,
+//   token: token,
+//   wins: wins 
+// })
 
 var winCombos = [
   [1, 2, 3],
@@ -31,17 +49,22 @@ var winCombos = [
 ];
 
 //Funcitons 
-function createPlayer(player, token, wins) {
+function createPlayer(name, token, wins) {
   return {
-    player: player,
+    name: name,
     token: token,
     wins: wins
   }
 }
 
 
-function startGame(currentPlayer){
-  h2.innerText = 'It\'s player 🌺\'s team.'
+function updatePlayerTurnText() {
+  if (playersTurn === player2) {
+      message.innerText = `It\'s player ${player2.token}\'s turn!`
+  } else {
+      message.innerText = `It\'s player ${player1.token}\'s turn!`
+  }
+}
+  
   
 
-}
